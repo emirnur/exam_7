5c6f2d1c7f70502e810d30f4e82ae00a82828f74
emirnur <nur.emirlan@gmail.com>
2019-10-19 16:40:52 +0600

    change fix bug with migrations

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -12,9 +12,16 @@ class Poll(models.Model):
 
 class Choice(models.Model):
     answer_option = models.TextField(max_length=200, null=False, blank=False, verbose_name='Вариант ответа')
-    poll = models.ForeignKey('Poll', on_delete=models.CASCADE, null=True, blank=False, verbose_name='Опрос',
+    poll = models.ForeignKey('webapp.Poll', on_delete=models.CASCADE, null=False, blank=False, verbose_name='Опрос',
                                  related_name='polls')
 
     def __str__(self):
         return self.answer_option
 
+
+class Answer(models.Model):
+    poll = models.ForeignKey('webapp.Poll', on_delete=models.CASCADE, null=False, blank=False, verbose_name='Опрос',
+                                 related_name='answer_poll')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    choice = models.ForeignKey('webapp.Choice', on_delete=models.CASCADE, null=False, blank=False, verbose_name='Вариант ответа',
+                             related_name='answer_poll')
