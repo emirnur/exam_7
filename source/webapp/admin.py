d3f20c2ebc8973f19fdc3424ddc24afce13f82cf
emirnur <nur.emirlan@gmail.com>
2019-10-19 12:21:11 +0600

    Add models and dump

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Choice, Poll
+
+admin.site.register(Poll)
+admin.site.register(Choice)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.TextField(max_length=200, null=False, blank=False, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    answer_option = models.TextField(max_length=200, null=False, blank=False, verbose_name='Вариант ответа')
+    poll = models.ForeignKey('Poll', on_delete=models.CASCADE, null=True, blank=False, verbose_name='Опрос',
+                                 related_name='polls')
+
+    def __str__(self):
+        return self.answer_option
