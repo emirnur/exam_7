3edd3fd35c7d30d27474d228ef633f1e89c687f2
emirnur <nur.emirlan@gmail.com>
2019-10-19 14:01:45 +0600

    add CRUD for Poll

---
+++ source/webapp/views/poll_views.py
@@ -0,0 +1,46 @@
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from webapp.forms import PollForm
+from webapp.models import Poll
+
+
+class IndexView(ListView):
+    template_name = 'poll/index.html'
+    context_object_name = 'polls'
+    model = Poll
+    ordering = ['-created_at']
+    paginate_by = 5
+    paginate_orphans = 1
+
+
+class PollView(DetailView):
+    template_name = 'poll/poll.html'
+    model = Poll
+    context_key = 'poll'
+
+
+class PollCreateView(CreateView):
+    form_class = PollForm
+    model = Poll
+    template_name = 'poll/create.html'
+
+    def get_success_url(self):
+        return reverse('poll_view', kwargs={'pk': self.object.pk})
+
+
+class PollUpdateView(UpdateView):
+    model = Poll
+    template_name = 'poll/update.html'
+    form_class = PollForm
+    context_object_name = 'poll'
+
+    def get_success_url(self):
+        return reverse('poll_view', kwargs={'pk': self.object.pk})
+
+
+class PollDeleteView(DeleteView):
+    template_name = 'poll/delete.html'
+    model = Poll
+    context_object_name = 'poll'
+    success_url = reverse_lazy('index')
